61f1072bc4779ac908018875b9a97dbaa5255661
D35YNC <d35ync+github@yandex.com>
2023-04-14 22:49:46 +1100

    Bad error msg fix

--- project94/cli_commands/help.py
+++ project94/cli_commands/help.py
@@ -12,14 +12,14 @@ class Help(BaseCommand):
 
     def __call__(self, *args, **kwargs):
         match args:
-            case ("/help", cmd):
-                if cmd := self._app.commands.get(cmd):
+            case ("/help", cmd_name):
+                if cmd := self._app.commands.get(cmd_name):
                     kwargs.get("print_info_callback", lambda x: print(f"[!] {x}"))(f"Help: {cmd}")
                     print(f"Description: {cmd.description}")
                     # print(f"Aliases: {', '.join(cmd.aliases)}.")
                     print(cmd.usage)
                 else:
-                    kwargs.get("print_warning_callback", lambda x: print(f"[!] {x}"))(f"Command {cmd} not found")
+                    kwargs.get("print_warning_callback", lambda x: print(f"[!] {x}"))(f"Command {cmd_name} not found")
             case _:
                 for cmd in self._app.commands:
                     print(f"{cmd:<20}{self._app.commands[cmd].description}")
