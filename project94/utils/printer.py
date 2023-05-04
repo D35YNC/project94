22bdc212c79b4c4d110196aa6fe8707606ff5bfb
D35YNC <d35ync+github@yandex.com>
2023-05-04 13:25:02 +1100

    Yet another one refactoring; Add time to 'log' messages; add ssl cert printing function

--- project94/utils/printer.py
+++ project94/utils/printer.py
@@ -1,44 +1,65 @@
+import datetime
+
+
+def _inject_time(func):
+    def wrapper(message):
+        t = datetime.datetime.now()
+        func(f"[{t.strftime('%H:%M:%S')}] {message}")
+    return wrapper
+
+
+def print_certificate(cert):
+    for k in cert:
+        print(f"{k[0].upper() + k[1:]}:", end=' ')
+        if isinstance(cert[k], tuple):
+            print()
+            for field in cert[k]:
+                print(f"{field[0][0]:>25}: {field[0][1]}")
+        else:
+            print(f"{cert[k]}")
+
+
 class Printer:
     colored = True
 
     @staticmethod
-    def _black(line):
+    def paint_black(line):
         return '\033[30m' + line + '\033[0m'
 
     @staticmethod
-    def _blue(line):
+    def paint_blue(line):
         return '\033[94m' + line + '\033[0m'
 
     @staticmethod
-    def _gray(line):
+    def paint_gray(line):
         return '\033[1;30m' + line + '\033[0m'
 
     @staticmethod
-    def _green(line):
+    def paint_green(line):
         return '\033[92m' + line + '\033[0m'
 
     @staticmethod
-    def _cyan(line):
+    def paint_cyan(line):
         return '\033[96m' + line + '\033[0m'
 
     @staticmethod
-    def _light_purple(line):
+    def paint_light_purple(line):
         return '\033[94m' + line + '\033[0m'
 
     @staticmethod
-    def _purple(line):
+    def paint_purple(line):
         return '\033[95m' + line + '\033[0m'
 
     @staticmethod
-    def _red(line):
+    def paint_red(line):
         return '\033[91m' + line + '\033[0m'
 
     @staticmethod
-    def _yellow(line):
+    def paint_yellow(line):
         return '\033[93m' + line + '\033[0m'
 
     @staticmethod
-    def _default(line):
+    def drop_color(line):
         return '\033[0m' + line + '\033[0m'
 
     @staticmethod
@@ -50,21 +71,25 @@ class Printer:
         return '\033[4m' + line + '\033[0m'
 
     @staticmethod
+    @_inject_time
     def info(message: str):
-        print(f"\r[*] {Printer._light_purple(message) if Printer.colored else message}", flush=True)
+        print(f"\r[*] {Printer.paint_light_purple(message) if Printer.colored else message}", flush=True)
 
     @staticmethod
+    @_inject_time
     def warning(message: str):
-        print(f"\r[!] {Printer._yellow(message) if Printer.colored else message}", flush=True)
+        print(f"\r[!] {Printer.paint_yellow(message) if Printer.colored else message}", flush=True)
 
     @staticmethod
+    @_inject_time
     def error(message: str):
-        print(f"\r[!!!] {Printer._red(message) if Printer.colored else message}", flush=True)
+        print(f"\r[ERROR] {Printer.paint_red(message) if Printer.colored else message}", flush=True)
 
     @staticmethod
+    @_inject_time
     def success(message: str):
-        print(f"\r[+] {Printer._green(message) if Printer.colored else message}", flush=True)
+        print(f"\r[+] {Printer.paint_green(message) if Printer.colored else message}", flush=True)
 
     @staticmethod
     def context(context: str):
-        return f"\r[{Printer._gray(context) if Printer.colored else context}]"
+        return f"\r[{Printer.paint_gray(context) if Printer.colored else context}]"
