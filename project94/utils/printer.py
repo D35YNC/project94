9999427f539ce163b38ed6c0ffc84f758595f561
D35YNC <d35ync+github@yandex.com>
2023-07-02 14:45:41 +1100

    inject current time into log messages;move print utils from builtins.py

--- project94/utils/printer.py
+++ project94/utils/printer.py
@@ -1,13 +1,6 @@
 import datetime
 
 
-def _inject_time(func):
-    def wrapper(message):
-        t = datetime.datetime.now()
-        func(f"[{t.strftime('%H:%M:%S')}] {message}")
-    return wrapper
-
-
 def print_certificate(cert):
     for k in cert:
         print(f"{k[0].upper() + k[1:]}:", end=' ')
@@ -17,6 +10,49 @@ def print_certificate(cert):
                 print(f"{field[0][0]:>25}: {field[0][1]}")
         else:
             print(f"{cert[k]}")
+
+
+def print_listener(listener):
+    print(f"Listener: {listener.name}")
+    print(f"Address: {listener.lhost}:{listener.lport}")
+    print(f"State: {listener.state}")
+    print(f"Autorun: {'Enabled' if listener.autorun else 'Disabled'}")
+    if listener.ssl_enabled:
+        print(f"SSL: Enabled")
+        stat = listener.ssl_context.cert_store_stats()
+        print(f"X509 CA loaded: {stat.get('x509_ca')}")
+        print(f"X509 loaded: {stat.get('x509')}")
+        print("CA certs:")
+        certs = listener.ssl_context.get_ca_certs()
+        for cert in certs:
+            print('-' * 0x2A)
+            print_certificate(cert)
+    else:
+        print(f"SSL: Disabled")
+    print(f"Active sessions: {len(listener.sockets)}")
+
+
+def print_session(session):
+    print(f"Hash: {session.hash}")
+    print(f"From: {session.rhost}:{session.rport}")
+    print(f"When: {datetime.datetime.fromtimestamp(session.timestamp).strftime('%m.%d %H:%M:%S')}")
+    print(f"Encoding: {session.encoding}")
+    if session.ssl_enabled:
+        print(f"SSL: Enabled")
+        print("Cert:")
+        print_certificate(session.cert)
+    else:
+        print(f"SSL: Disabled")
+
+    for ext in session.extended_info:
+        print(f"{ext}: {session.extended_info[ext]}")
+
+
+def _inject_time(func):
+    def wrapper(message):
+        t = datetime.datetime.now()
+        func(f"[{t.strftime('%H:%M:%S')}] {message}")
+    return wrapper
 
 
 class Printer:
