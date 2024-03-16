282441a2c457ea10d67554da7582e25c4a4dee66
D35YNC <d35ync+github@yandex.com>
2024-03-16 15:57:12 +1100

    Some naming changes & annotations

--- project94/listener.py
+++ project94/listener.py
@@ -6,7 +6,7 @@ import ssl
 from project94.utils.logs import create_full_logger
 
 
-class ListenerStateEnum(enum.Enum):
+class ListenerStateId(enum.Enum):
     Unknown = -1,
     Stopped = 0,
     Running = 1
@@ -35,16 +35,22 @@ class ListenerState(metaclass=abc.ABCMeta):
 
 
 class Running(ListenerState):
+    def __init__(self, listener):
+        super().__init__(listener, ListenerStateId.Running)
+
     def start(self):
         raise ListenerStartError(f"{self._listener} already running")
 
     def stop(self):
         self._listener.listen_socket.shutdown(socket.SHUT_RDWR)
         self._listener.listen_socket.close()
-        self._listener.change_state(ListenerStateEnum.Stopped)
+        self._listener.change_state(ListenerStateId.Stopped)
 
 
 class Stopped(ListenerState):
+    def __init__(self, listener):
+        super().__init__(listener, ListenerStateId.Stopped)
+
     def start(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -54,7 +60,7 @@ class Stopped(ListenerState):
             raise ListenerStartError(str(ex))
         else:
             sock.listen(0x10)
-            self._listener.change_state(ListenerStateEnum.Running)
+            self._listener.change_state(ListenerStateId.Running)
             return sock
 
     def stop(self):
@@ -62,6 +68,9 @@ class Stopped(ListenerState):
 
 
 class Unknown(ListenerState):
+    def __init__(self, listener):
+        super().__init__(listener, ListenerStateId.Unknown)
+
     def start(self) -> socket.socket | None:
         raise ListenerStartError(f"{self._listener} need setup")
 
@@ -82,24 +91,24 @@ class Listener:
         self.__lhost = lhost
         self.__lport = lport
         self.__socket: socket.socket = None
-        self.__accepted_sockets = []
+        self.__accepted_sockets: list[socket.socket] = []
         self.__ssl_context: ssl.SSLContext = None
         self.__ca = []
         self.__cert = []
         self.__autorun = autorun
         self.__drop_duplicates = drop_duplicates
         self.__logger = create_full_logger(self.__name)
-        self.__states = {ListenerStateEnum.Running: Running(self, ListenerStateEnum.Running),
-                         ListenerStateEnum.Stopped: Stopped(self, ListenerStateEnum.Stopped),
-                         ListenerStateEnum.Unknown: Unknown(self, ListenerStateEnum.Unknown)}
+        self.__states = {ListenerStateId.Running: Running(self),
+                         ListenerStateId.Stopped: Stopped(self),
+                         ListenerStateId.Unknown: Unknown(self)}
         if enable_ssl:
-            self.__state = self.__states[ListenerStateEnum.Unknown]
+            self.__state = self.__states[ListenerStateId.Unknown]
             self.__ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
             self.__ssl_context.verify_mode = ssl.VerifyMode.CERT_REQUIRED
             self.__ssl_context.set_ciphers("ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES256-SHA384")
             self.__logger.info(f"SSL context created")
         else:
-            self.__state = self.__states[ListenerStateEnum.Stopped]
+            self.__state = self.__states[ListenerStateId.Stopped]
 
         self.__logger.info(f"Seems initialized: {self} {self.state}")
 
@@ -112,8 +121,8 @@ class Listener:
             else:
                 self.__ca.append(cafile)
 
-                if 0 < len(self.__ca) and 0 < len(self.__cert) and self.__state.enum_obj is not ListenerStateEnum.Running:
-                    self.__state = self.__states[ListenerStateEnum.Stopped]
+                if 0 < len(self.__ca) and 0 < len(self.__cert) and self.__state.enum_obj is not ListenerStateId.Running:
+                    self.__state = self.__states[ListenerStateId.Stopped]
                 return True
         return None
 
@@ -126,8 +135,8 @@ class Listener:
             else:
                 self.__cert.append((certfile, keyfile))
 
-                if 0 < len(self.__ca) and 0 < len(self.__cert) and self.__state.enum_obj is not ListenerStateEnum.Running:
-                    self.__state = self.__states[ListenerStateEnum.Stopped]
+                if 0 < len(self.__ca) and 0 < len(self.__cert) and self.__state.enum_obj is not ListenerStateId.Running:
+                    self.__state = self.__states[ListenerStateId.Stopped]
                 return True
         return None
 
@@ -141,7 +150,7 @@ class Listener:
         self.stop()
         return self.start()
 
-    def change_state(self, new_state: ListenerStateEnum) -> None:
+    def change_state(self, new_state: ListenerStateId) -> None:
         if new_state in self.__states:
             self.__state = self.__states[new_state]
 
@@ -155,7 +164,7 @@ class Listener:
 
     @property
     def is_running(self) -> bool:
-        return self.__state is self.__states[ListenerStateEnum.Running] and self.__socket is not None
+        return self.__state is self.__states[ListenerStateId.Running] and self.__socket is not None
 
     @property
     def listen_socket(self) -> socket.socket:
